b10f8595e76b8de24a67d3fc9f36077caf2e4984
JACSONQIU\qyjbe <qyjbeijing@gmail.com>
2025-12-17 18:57:26 +0800

    MOD: 尝试将tags导入

--- src/statistic/statistic.service.ts
+++ src/statistic/statistic.service.ts
@@ -24,7 +24,7 @@ export class StatisticService {
             take: 100,
             order: { createdAt: 'DESC' },
             where: {
-                tags: ArrayContains([]),
+                tags: ArrayContains(logDto.tags),
             }
         });
         return {
