b118cd7335ca06d827cd4f690a8af4aaf52f2037
JACSONQIU\qyjbe <qyjbeijing@gmail.com>
2025-12-17 17:24:39 +0800

    MOD: 处理json数组

--- src/statistic/dto/log.dto.ts
+++ src/statistic/dto/log.dto.ts
@@ -13,7 +13,13 @@ export class LogDto {
     @Transform(({ value }) => {
         // 如果前端传 ?tags=fly,pve
         if (typeof value === 'string') {
-            return value.split(',');
+            try {
+                const parsed = JSON.parse(value);
+                if (Array.isArray(parsed)) return parsed;
+            } catch (e) {
+                // 如果不是 JSON，就按逗号分隔
+                return value.split(',');
+            }
         }
         // 如果前端传 ?tags=fly&tags=pve，则 value 会是数组
         return value;
