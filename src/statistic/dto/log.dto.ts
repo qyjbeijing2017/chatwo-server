1b894d71b6946e68e2eb4c821eee51e921b4fc05
JACSONQIU\qyjbe <qyjbeijing@gmail.com>
2025-12-17 20:26:20 +0800

    MOD:  尝试重新解析一下

--- src/statistic/dto/log.dto.ts
+++ src/statistic/dto/log.dto.ts
@@ -13,7 +13,7 @@ export class LogDto {
     @Transform(({ value }) => {
         // 如果前端传 ?tags=fly,pve
         if (typeof value === 'string') {
-            return value.split(',');
+            return JSON.parse(value);
         }
         // 如果前端传 ?tags=fly&tags=pve，则 value 会是数组
         return value;
