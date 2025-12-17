c46b78b97858fab144230c809394dfcc0c83ec0a
JACSONQIU\qyjbe <qyjbeijing@gmail.com>
2025-12-17 18:29:59 +0800

    MOD: 确保一个tag也能运行

--- src/statistic/dto/log.dto.ts
+++ src/statistic/dto/log.dto.ts
@@ -10,6 +10,14 @@ export class LogDto {
     })
     @IsArray()
     @IsString({ each: true })
+    @Transform(({ value }) => {
+        // 如果前端传 ?tags=fly,pve
+        if (typeof value === 'string') {
+            return value.split(',');
+        }
+        // 如果前端传 ?tags=fly&tags=pve，则 value 会是数组
+        return value;
+    })
     tags: string[];
 
     @ApiPropertyOptional()
