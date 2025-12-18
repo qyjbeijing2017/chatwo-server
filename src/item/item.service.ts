9c849621883688de34908297f3ca39376e1ca58c
JACSONQIU\qyjbe <qyjbeijing@gmail.com>
2025-12-19 04:56:14 +0800

    ADD: 倒叙

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -28,6 +28,9 @@ export class ItemService {
           type: ContainerType.chest,
           owner: { nakamaId: account.custom_id },
         },
+      },
+      order: {
+        updatedAt: 'DESC',
       }
     });
   }
