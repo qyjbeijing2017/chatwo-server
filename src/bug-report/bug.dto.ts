a46d95056cdd8698d5691dc5925787b0f4251718
JACSONQIU\qyjbe <qyjbeijing@gmail.com>
2026-03-04 09:29:12 +0800

    DEL: unused proper

--- src/bug-report/bug.dto.ts
+++ src/bug-report/bug.dto.ts
@@ -2,23 +2,6 @@ import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class BugDto {
-    // @ApiProperty()
-    // @IsString()
-    // customId: string;
-
-    @ApiProperty()
-    @IsInt()
-    amount: number;
-
-    @ApiProperty()
-    @IsString()
-    reason: string;
-
-    @ApiPropertyOptional()
-    @IsString({ each: true })
-    tags?: string[];
-
-
     @ApiProperty()
     @IsNotEmpty()
     @IsInt()
